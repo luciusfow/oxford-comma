3b7642316700ad2aad7c9025606c4b394b7adc8b
Lucius Fowler <luciusfow@gmail.com>
2020-12-15 20:55:38 -0500

    commit tree 28b03dd59309b427e938d25cd79a34df5f2787f0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+    if array.length == 2
+    array.join(" and ")
+    elsif array.length >= 3
+        [array[0..-2].join(', '), array.last].join(', and ')
+    else array.join
+    end
 end
